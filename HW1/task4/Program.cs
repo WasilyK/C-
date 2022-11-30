18cab56456e53d64460a0143a3c137409a51abf6
WasilyK <vasua84@yandex.ru>
2022-11-30 15:15:02 +0900

    Home Work 1

---
+++ HW1/task4/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Напишите программу, которая принимает на вход 
+    три числа и выдаёт максимальное из этих чисел. */
+    
+Console.WriteLine("Find MAX");
+
+int[] numbs = new int[3];
+
+Console.Write("Пожалуйста, введите первое число и нажмите \"Enter\": ");
+numbs[0] = int.Parse(Console.ReadLine());
+
+Console.Write("Пожалуйста, введите второе число и нажмите \"Enter\": ");
+numbs[1] = int.Parse(Console.ReadLine());
+
+Console.Write("Пожалуйста, введите третье число и нажмите \"Enter\": ");
+numbs[2] = int.Parse(Console.ReadLine());
+
+int max = numbs[0];
+
+for(int i = 0; i < numbs.Length; i++) {
+    if(numbs[i] > max) {
+        max = numbs[i];
+    }
+}
+
+Console.WriteLine("Максимальное число: " + max);
