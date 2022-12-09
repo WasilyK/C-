5a91f1d4783dbeaaa11c8207d43411a6abe8b92e
WasilyK <vasua84@yandex.ru>
2022-12-09 17:23:12 +0900

    homework4

---
+++ HW4/task25/Program.cs
@@ -0,0 +1,21 @@
+﻿/*  
+*   Напишите цикл, который принимает на вход два числа (A и B) 
+*   и возводит число A в натуральную степень B.
+*   3, 5 -> 243 (3⁵)
+*   2, 4 -> 16 
+*/
+
+Console.Write("Enter a number: ");
+int numb = int.Parse(Console.ReadLine());
+Console.Write("Enter a power: ");
+int pow = int.Parse(Console.ReadLine());
+
+Console.WriteLine(powerCycle(numb, pow));
+
+int powerCycle(int numb, int pow) {
+    int res = 1;
+    for(int i  = 0; i < pow; i++) {
+        res *= numb;
+    }
+    return res;
+}
