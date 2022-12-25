049730a935d221fb6fb1e74a2368ce5b26ed3007
WasilyK <vasua84@yandex.ru>
2022-12-26 06:35:07 +0900

    homework9

---
+++ HW9/task66/Program.cs
@@ -0,0 +1,28 @@
+﻿/**
+*   Задайте значения M и N. Напишите программу, 
+*   которая найдёт сумму натуральных элементов в промежутке от M до N.
+*   M = 1; N = 15 -> 120
+*   M = 4; N = 8. -> 30
+*/
+using user_interaction;
+Input input = new Input();
+
+int m = input.GetNumberFromUser("Enter a first number: ");
+int n = input.GetNumberFromUser("Enter a second number: ");
+
+Console.WriteLine(Sum(GetMax(m, n), GetMin(m, n)));
+
+int Sum(int max, int min) {
+    if(max <= 0 || min == max - 1) {
+        return 0;
+    }
+    return (max - 1) + Sum(--max, min);
+}
+
+int GetMax(int m, int n) {
+    return m > n ? m : n;
+}
+
+int GetMin(int m, int n) {
+    return m > n ? n : m;
+}
---
+++ HW9/task68/Program.cs
@@ -0,0 +1,21 @@
+﻿/**
+*   Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+*   Даны два неотрицательных числа m и n.
+*   m = 2, n = 3 -> A(m,n) = 9
+*   m = 3, n = 2 -> A(m,n) = 29
+*/
+
+Console.WriteLine(AkkFunc(3,2));
+
+int AkkFunc(int m, int n) {
+    if(m < 0 || n < 0) {
+        throw new ArgumentException("negative number");
+    }
+    if(m == 0) {
+        return n + 1;
+    }
+    if(n == 0) {
+        return AkkFunc(m - 1, 1);
+    }
+    return AkkFunc(m - 1, AkkFunc(m, n - 1));
+}
